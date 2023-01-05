d7e5459f8d93fa43bca524a11c85b381240b340e
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2023-01-05 13:41:01 +0100

    I'm stupid

--- src/main/java/de/kaitokuntatsu/parser/ScannerAndParser.java
+++ src/main/java/de/kaitokuntatsu/parser/ScannerAndParser.java
@@ -1,63 +1,82 @@
 package de.kaitokuntatsu.parser;
 
-import de.kaitokuntatsu.utils.SortUtils;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import de.kaitokuntatsu.datastructures.*;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class ScannerAndParser {
 
-    static final String[] terminals = {"[","]", "RE","LI", "WH", "VW"};
+    static final String[] COMMANDS = {"RE","LI", "WH", "VW"};
 
-    public List<Token> scan(String pInput) {
-        List<Token> lTokenList = new List<>();
-        String[] lPotentialTokens = pInput.split(" ");
+    public List<Befehl> parse(String pInput) {
+        return parse(pInput.split(" "), 0);
+    }
 
-        for (int i = 0; i < lPotentialTokens.length; ++i) {
-            if (getNumber(lPotentialTokens[i]) > -1)
-                lTokenList.append(new Token(Token.Type.ZAHL, lPotentialTokens[i]));
-            else if (isTerminal(lPotentialTokens[i]))
-                lTokenList.append(new Token(Token.Type.ZAHL, lPotentialTokens[i]));
-            else
+    public List<Befehl> parse(String[] pPotentialCommands, int pStartIndex) {
+        List<Befehl> lCommands = new List<>();
+
+        for (int i = pStartIndex; i < pPotentialCommands.length; ++i) {
+            if (isCommand(pPotentialCommands[i]) && i + 1 < pPotentialCommands.length) {
+                int lValue = parseToInt(pPotentialCommands[i+1]);
+                if (lValue == -1) {
+                    return null;
+                }
+
+                if (pPotentialCommands[i].equals("WH")) {
+                    if (!pPotentialCommands[i+2].equals("[")) {
+                        return null;
+                    }
+
+                    List<Befehl> lCommandsInLoop = parse(pPotentialCommands, i+3);
+                    for (int j = 0; j < lValue; ++j) {
+                        lCommandsInLoop.toFirst();
+                        while (lCommandsInLoop.hasAccess()) {
+                            lCommands.append(lCommandsInLoop.getContent());
+                        }
+                    }
+                }
+                else {
+                    lCommands.append(new Befehl(pPotentialCommands[i], lValue));
+                }
+
+                ++i;
+            }
+            else if (pPotentialCommands[i].equals("]")) {
+                return lCommands;
+            }
+            else {
                 return null;
+            }
         }
 
-        return lTokenList;
+        return lCommands;
     }
 
-    public boolean parse(List<Token> pTokenList) {
-        return false;
-    }
-
-    private boolean isTerminal(String pPotentialTerminal) {
-        for (String term : terminals) {
-            if (term.equals(pPotentialTerminal))
+    private boolean isCommand(String pPotentialCommand) {
+        for (String lCmd : COMMANDS) {
+            if (lCmd.equals(pPotentialCommand)) {
                 return true;
+            }
         }
         return false;
     }
 
-    private int getNumber(String pStr) {
-        try {
-            return Integer.parseInt(pStr);
-        } catch (NumberFormatException ex) {
-            return -1;
+    private int parseToInt(String pStr) {
+        char[] lChars = pStr.toCharArray();
+        for (char c : lChars) {
+            if (c < '0' || c > '9')
+                return -1;
         }
-    }
-
-    public int getIndex(String pElement, String[] pArr) {
-        for (int i = pArr.length-1; i >= 0; --i) {
-            if (pArr[i].equals(pElement)) return i;
-        }
-        return -1;
+        return Integer.parseInt(pStr);
     }
 
     public static void main(String[] args) throws IOException {
-
-    }
+        ScannerAndParser sut = new ScannerAndParser();
+        List<Befehl> cmds = sut.parse("VW 5 WH 5 [ LI 5 ]");
+        cmds.toFirst();
+        while (cmds.hasAccess()) {
+            System.out.println(cmds.getContent().typ() + " + " + cmds.getContent().wert());
+            cmds.next();
+        }
+     }
 }
 
